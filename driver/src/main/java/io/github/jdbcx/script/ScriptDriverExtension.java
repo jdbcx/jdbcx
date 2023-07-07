60b03a49c973a098f116a3bf7d2c98ae57790559
Zhichun Wu <zhicwu@gmail.com>
2023-07-07 17:34:42 +0800

    Reset default scripting language as needed

--- driver/src/main/java/io/github/jdbcx/script/ScriptDriverExtension.java
+++ driver/src/main/java/io/github/jdbcx/script/ScriptDriverExtension.java
@@ -21,17 +21,52 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
+import io.github.jdbcx.Checker;
 import io.github.jdbcx.ConnectionListener;
+import io.github.jdbcx.Constants;
 import io.github.jdbcx.DriverExtension;
+import io.github.jdbcx.Logger;
+import io.github.jdbcx.LoggerFactory;
 import io.github.jdbcx.Option;
 import io.github.jdbcx.Scripting;
 
 public class ScriptDriverExtension implements DriverExtension {
+    private static final Logger log = LoggerFactory.getLogger(ScriptDriverExtension.class);
+
+    private Option getLanguageOption(Properties props) {
+        String defaultLang = Scripting.OPTION_LANGUAGE.getDefaultValue();
+        String[] languages = new Scripting(null, props, false, null).getSupportedLanguages();
+        Option.Builder builder = Scripting.OPTION_LANGUAGE.update();
+        if (languages.length == 0) {
+            if (!Checker.isNullOrEmpty(defaultLang)) {
+                builder.defaultValue(Constants.EMPTY_STRING);
+                log.warn("The default scripting language has been reset from \"%s\" to empty "
+                        + "because no ScriptEngineFactory was found in the classpath.", defaultLang);
+            }
+        } else {
+            builder.choices(languages);
+
+            boolean exist = false;
+            for (String lang : languages) {
+                if (defaultLang.equals(lang)) {
+                    exist = true;
+                    break;
+                }
+            }
+            if (!exist) {
+                builder.defaultValue(languages[0]);
+                log.warn("The default scripting language has been reset from \"%s\" to \"%s\" "
+                        + "because no corresponding ScriptEngineFactory was found in the classpath.", defaultLang,
+                        languages[0]);
+            }
+        }
+        return builder.build();
+    }
+
     @Override
     public List<Option> getOptions(Properties props) {
         List<Option> list = new ArrayList<>();
-        list.add(Scripting.OPTION_LANGUAGE.update()
-                .choices(new Scripting(null, props, false, null).getSupportedLanguages()).build());
+        list.add(getLanguageOption(props));
         list.add(Scripting.OPTION_BINDING_ERROR);
         list.add(ScriptConnectionListener.OPTION_VAR_CONNECT);
         list.add(ScriptConnectionListener.OPTION_VAR_HELPER);
