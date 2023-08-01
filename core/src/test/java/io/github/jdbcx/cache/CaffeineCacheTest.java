5c37733e58513e7b84d3bfd5b45e24cd934a45d6
Zhichun Wu <zhicwu@gmail.com>
2023-08-01 22:43:43 +0800

    refine unit test

--- core/src/test/java/io/github/jdbcx/cache/CaffeineCacheTest.java
+++ core/src/test/java/io/github/jdbcx/cache/CaffeineCacheTest.java
@@ -39,10 +39,6 @@ public class CaffeineCacheTest {
 
         AutoClosableResource res3 = cache.get("3");
         Assert.assertEquals(res3.key, "3");
-        Assert.assertEquals(res3.closed.get(), false);
-        Assert.assertEquals(res2.closed.get(), false);
-        Assert.assertEquals(res1.closed.get(), false);
-
         Cache<String, AutoClosableResource> c = (Cache<String, AutoClosableResource>) cache.unwrap(Cache.class);
         c.cleanUp();
         Assert.assertEquals(res3.closed.get(), false);
