341499a8fab9ea7f9c8d329fb0171c6f68838ae5
Zhichun Wu <zhicwu@gmail.com>
2024-02-03 10:05:45 +0800

    clean up code

--- driver/src/main/java/io/github/jdbcx/driver/QueryParser.java
+++ driver/src/main/java/io/github/jdbcx/driver/QueryParser.java
@@ -417,17 +417,17 @@ public final class QueryParser {
                 if (nextChar == ch && !escaped) { // executable block with output: {{ ... }}
                     int index = indexOf(query, i + 1, "}}", escapeChar);
                     if (index > 0) {
-                        int endIndex = index;
                         for (int k = index; k < len; k++) { // be greedy
                             if (query.charAt(k) == '}') {
                                 index++;
-                                endIndex = index - 2;
                             } else {
                                 break;
                             }
                         }
-                        if (endIndex == index) {
-                            endIndex -= 2;
+                        if (query.charAt(i + 1) == '-') {
+                            log.debug("Skip executable block: %s", query.substring(i - 1, index));
+                        } else {
+                            int endIndex = index - 2;
                             for (int k = endIndex - 1; k > i; k--) {
                                 if (Character.isWhitespace(query.charAt(k))) {
                                     endIndex = k;
@@ -435,10 +435,6 @@ public final class QueryParser {
                                     break;
                                 }
                             }
-                        }
-                        if (query.charAt(i + 1) == '-') {
-                            log.debug("Skip executable block: %s", query.substring(i - 1, index));
-                        } else {
                             addExecutableBlock(builder, query.substring(i + 1, endIndex), vars, true, parts, blocks);
                         }
                         i = index - 1;
@@ -448,29 +444,18 @@ public final class QueryParser {
                     }
                 } else if (nextChar == '%' && !escaped) { // executable block: {% ... %}
                     int index = indexOf(query, i + 1, "%}", escapeChar);
-                    int endIndex = index;
-                    for (int k = index; k < len; k++) { // be greedy
-                        if (query.charAt(k) == '%') {
-                            index++;
-                            endIndex = index - 2;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (endIndex == index) {
-                        endIndex -= 2;
-                        for (int k = endIndex - 1; k > i; k--) {
-                            if (Character.isWhitespace(query.charAt(k))) {
-                                endIndex = k;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
                     if (index > 0) {
                         if (query.charAt(i + 1) == '-') {
                             log.debug("Skip executable block: %s", query.substring(i - 1, index));
                         } else {
+                            int endIndex = index - 2;
+                            for (int k = endIndex - 1; k > i; k--) {
+                                if (Character.isWhitespace(query.charAt(k))) {
+                                    endIndex = k;
+                                } else {
+                                    break;
+                                }
+                            }
                             addExecutableBlock(builder, query.substring(i + 1, endIndex), vars, false, parts, blocks);
                         }
                         i = index - 1;
