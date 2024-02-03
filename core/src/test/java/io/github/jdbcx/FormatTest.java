a7607612e07a38b6328e38a7f1c606b56eb825b4
Zhichun Wu <zhicwu@gmail.com>
2024-02-03 14:50:20 +0800

    add compression and format definitions (#32)

---
+++ core/src/test/java/io/github/jdbcx/FormatTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022-2024, Zhichun Wu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.jdbcx;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FormatTest {
+    static final Format defaultFormat = Format.TSV;
+
+    @Test(groups = { "unit" })
+    public void testFromFileExtension() {
+        Assert.assertNull(Format.fromFileExtension(null, null));
+        Assert.assertNull(Format.fromFileExtension("", null));
+        Assert.assertNull(Format.fromFileExtension("unknown", null));
+
+        Assert.assertEquals(Format.fromFileExtension(null, defaultFormat), defaultFormat);
+        Assert.assertEquals(Format.fromFileExtension("", defaultFormat), defaultFormat);
+        Assert.assertEquals(Format.fromFileExtension("unknown", defaultFormat), defaultFormat);
+
+        for (Format f : Format.values()) {
+            Assert.assertEquals(Format.fromFileExtension(f.fileExtension(false), null), f);
+        }
+    }
+
+    @Test(groups = { "unit" })
+    public void testFromFileName() {
+        Assert.assertNull(Format.fromFileName(null));
+        Assert.assertNull(Format.fromFileName(""));
+        Assert.assertNull(Format.fromFileName("unknown"));
+
+        Assert.assertEquals(Format.fromFileName("my.json"), Format.JSON);
+        Assert.assertNull(Format.fromFileName("my.csv.gz"));
+
+        for (Format f : Format.values()) {
+            Assert.assertEquals(Format.fromFileName("a" + f.fileExtension()), f);
+        }
+    }
+
+    @Test(groups = { "unit" })
+    public void testFromMimeType() {
+        Assert.assertEquals(Format.fromMimeType(null), defaultFormat);
+        Assert.assertEquals(Format.fromMimeType(""), defaultFormat);
+        Assert.assertEquals(Format.fromMimeType("*/csv"), defaultFormat);
+        Assert.assertEquals(Format.fromMimeType("*/*"), defaultFormat);
+        Assert.assertEquals(Format.fromMimeType("text/*"), defaultFormat);
+        Assert.assertEquals(Format.fromMimeType("unknown"), defaultFormat);
+
+        for (Format f : Format.values()) {
+            Assert.assertEquals(Format.fromMimeType(f.mimeType()), f);
+        }
+
+        Assert.assertEquals(Format.fromMimeType("Application/JSON"), Format.JSON);
+        Assert.assertEquals(Format.fromMimeType("application/bson;q=0.6,text/csv;q=0.4"), Format.BSON);
+        Assert.assertEquals(Format.fromMimeType("text/csv"), Format.CSV);
+        Assert.assertEquals(
+                Format.fromMimeType("text/html, application/unknown, application/vnd.apache.parquet;q=0.9, image/webp"),
+                Format.PARQUET);
+    }
+}
