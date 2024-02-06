289bbaf8d455ee921d66c26007e364f0919482a7
Zhichun Wu <zhicwu@gmail.com>
2024-02-06 21:41:09 +0800

    add datasource and logging config files for bridge server

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,14 +3,14 @@
 set -e
 
 : ${JDBCX_OPTS:=""}
-: ${SERVER_OPTS:="-Dserver.host=0.0.0.0"}
+: ${SERVER_OPTS:=""}
 
 run_cli() {
     java ${JDBCX_OPTS} -cp jdbcx.jar io.github.jdbcx.Main "$@"
 }
 
 start_server() {
-	java ${SERVER_OPTS} -cp jdbcx.jar:slf4j-api.jar:slf4j-simple.jar io.github.jdbcx.server.BridgeServer
+	java ${SERVER_OPTS} -cp jdbcx.jar:slf4j-api.jar:slf4j-simple.jar:. io.github.jdbcx.server.BridgeServer
 }
 
 # start clickhouse server
