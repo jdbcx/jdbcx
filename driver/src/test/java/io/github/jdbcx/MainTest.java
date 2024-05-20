92105352d200f65d9bb2e93c8ec098af52aec946
Zhichun Wu <zhicwu@gmail.com>
2024-05-20 21:43:54 +0800

    use jdbcx url in tests

--- driver/src/test/java/io/github/jdbcx/MainTest.java
+++ driver/src/test/java/io/github/jdbcx/MainTest.java
@@ -30,12 +30,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainTest {
+    static String[][] toArray(List<List<String[]>> list) {
+        List<String[]> result = new LinkedList<>();
+        for (List<String[]> l : list) {
+            result.addAll(l);
+        }
+        return result.toArray(new String[0][2]);
+    }
+
     @Test(groups = { "unit" })
     public void testCloseConnection() throws SQLException {
         Main.closeQuietly(null);
         Main.closeQuietly(new ByteArrayInputStream(new byte[1]));
 
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite::memory:", null)) {
+        try (Connection conn = DriverManager.getConnection("jdbcx:sqlite::memory:", null)) {
             Assert.assertFalse(conn.isClosed());
             Main.closeQuietly(conn);
             Assert.assertTrue(conn.isClosed());
@@ -47,7 +55,8 @@ public class MainTest {
         Assert.assertNull(Main.getOrCreateConnection(null, null, null, -1, null, false));
         Assert.assertNull(Main.getOrCreateConnection(null, null, null, 0, null, true));
 
-        Connection conn = Main.getOrCreateConnection("jdbc:sqlite::memory:", null, null, 1000, "select 2", false);
+        Connection conn = Main.getOrCreateConnection("jdbcx:sqlite::memory:", null, null, 1000, "select 2",
+                false);
         Assert.assertNotNull(conn);
         for (int i = 0; i < 3; i++) {
             Assert.assertFalse(conn.isClosed());
@@ -58,28 +67,22 @@ public class MainTest {
                 props = new Properties();
             }
             Assert.assertEquals(conn,
-                    Main.getOrCreateConnection("jdbc:sqlite::memory:", props, conn, 1000, "select 2", false));
+                    Main.getOrCreateConnection("jdbcx:sqlite::memory:", props, conn, 1000,
+                            "select 2", false));
         }
 
         for (int i = 0; i < 100; i++) {
             Assert.assertFalse(conn.isClosed());
             conn.close();
             Assert.assertTrue(conn.isClosed());
-            Connection newConn = Main.getOrCreateConnection("jdbc:sqlite::memory:", null, conn, 1000, "select 3",
+            Connection newConn = Main.getOrCreateConnection("jdbcx:sqlite::memory:", null, conn, 1000,
+                    "select 3",
                     false);
             Assert.assertNotEquals(conn, newConn);
             Assert.assertTrue(conn.isClosed());
             Assert.assertFalse(newConn.isClosed());
             conn = newConn;
         }
-    }
-
-    static String[][] toArray(List<List<String[]>> list) {
-        List<String[]> result = new LinkedList<>();
-        for (List<String[]> l : list) {
-            result.addAll(l);
-        }
-        return result.toArray(new String[0][2]);
     }
 
     @Test(groups = { "unit" })
@@ -93,47 +96,65 @@ public class MainTest {
                 new String[][] { { "n", "q" } });
 
         Assert.assertEquals(
-                toArray(Main.splitTasks(Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" }), 1)),
+                toArray(Main.splitTasks(
+                        Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" }),
+                        1)),
                 new String[][] { { "n1", "q1" }, { "n2", "q2" } });
         Assert.assertEquals(
-                toArray(Main.splitTasks(Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" }), 2)),
+                toArray(Main.splitTasks(
+                        Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" }),
+                        2)),
                 new String[][] { { "n1", "q1" }, { "n2", "q2" } });
 
         Assert.assertEquals(
-                toArray(Main.splitTasks(Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" },
-                        new String[] { "n3", "q3" }), 1)),
+                toArray(Main.splitTasks(
+                        Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" },
+                                new String[] { "n3", "q3" }),
+                        1)),
                 new String[][] { { "n1", "q1" }, { "n2", "q2" }, { "n3", "q3" } });
         Assert.assertEquals(
-                toArray(Main.splitTasks(Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" },
-                        new String[] { "n3", "q3" }), 2)),
+                toArray(Main.splitTasks(
+                        Arrays.asList(new String[] { "n1", "q1" }, new String[] { "n2", "q2" },
+                                new String[] { "n3", "q3" }),
+                        2)),
                 new String[][] { { "n1", "q1" }, { "n3", "q3" }, { "n2", "q2" } });
     }
 
     @Test(groups = { "unit" })
     public void testExecuteQueries() throws IOException, SQLException {
-        Assert.assertFalse(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null, null, null, null, false));
-        Assert.assertFalse(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null, Collections.emptyList(), null,
+        final String url = "jdbcx:sqlite::memory:";
+        Assert.assertFalse(Main.executeQueries(url, false, 1000, null, null, null, null,
+                false));
+        Assert.assertFalse(Main.executeQueries(url, false, 1000, null,
+                Collections.emptyList(), null,
                 null, false));
 
-        Assert.assertTrue(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null,
+        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
                 Collections.singletonList(new String[] { "first", "select 1" }), null, null, false));
-        Assert.assertTrue(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null,
+        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
                 Collections.singletonList(
-                        new String[] { "first", "SELECT name FROM sqlite_master WHERE type='invalid_table'" }),
+                        new String[] { "first",
+                                "SELECT name FROM sqlite_master WHERE type='invalid_table'" }),
                 null, null, false));
-        Assert.assertThrows(SQLException.class, () -> Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null,
-                Collections.singletonList(new String[] { "first", "select '" }), null, null, false));
+        Assert.assertThrows(SQLException.class,
+                () -> Main.executeQueries(url, false, 1000, null,
+                        Collections.singletonList(new String[] { "first", "select '" }), null,
+                        null, false));
 
-        Assert.assertTrue(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null,
-                Arrays.asList(new String[] { "first", "select 1" }, new String[] { "2nd", "select 2" }), null, null,
+        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
+                Arrays.asList(new String[] { "first", "select 1" }, new String[] { "2nd", "select 2" }),
+                null, null,
                 false));
-        Assert.assertTrue(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null,
-                Arrays.asList(new String[] { "first", "SELECT name FROM sqlite_master WHERE type='invalid_table'" },
+        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
+                Arrays.asList(new String[] { "first",
+                        "SELECT name FROM sqlite_master WHERE type='invalid_table'" },
                         new String[] { "2nd", "select 2" }),
                 null, null, false));
-        Assert.assertTrue(Main.executeQueries("jdbc:sqlite::memory:", false, 1000, null,
-                Arrays.asList(new String[] { "ddl", "create table if not exists test_execute_queries(a VARCHAR(30))" },
-                        new String[] { "1st", "SELECT name FROM sqlite_master WHERE type='test_execute_queries'" },
+        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
+                Arrays.asList(new String[] { "ddl",
+                        "create table if not exists test_execute_queries(a VARCHAR(30))" },
+                        new String[] { "1st",
+                                "SELECT name FROM sqlite_master WHERE type='test_execute_queries'" },
                         new String[] { "2nd", "insert into test_execute_queries values('x')" },
                         new String[] { "3rd", "select a from test_execute_queries" }),
                 null, null, true));
