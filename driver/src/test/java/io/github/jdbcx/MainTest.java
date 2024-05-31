1aec67a7d57b7889212a66a46ef7f993f18c09b2
Zhichun Wu <zhicwu@gmail.com>
2024-05-31 22:10:58 +0800

    enhance CLI to handle format and compression same way as in bridge server

--- driver/src/test/java/io/github/jdbcx/MainTest.java
+++ driver/src/test/java/io/github/jdbcx/MainTest.java
@@ -30,6 +30,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainTest {
+    static final String DEFAULT_CONNECTION_URL = "jdbcx:sqlite::memory:";
+
     static String[][] toArray(List<List<String[]>> list) {
         List<String[]> result = new LinkedList<>();
         for (List<String[]> l : list) {
@@ -38,25 +40,102 @@ public class MainTest {
         return result.toArray(new String[0][2]);
     }
 
-    @Test(groups = { "private" })
+    @Test(groups = { "unit" })
+    public void testCustomArguments() throws IOException {
+        Properties props = new Properties();
+        props.setProperty("x", "y");
+        String[][] queries = new String[][] { { "q1", "select 'q'" }, { "q 1", "select 2" } };
+        Main.Arguments args = new Main.Arguments(10L, 9L, true, "12.3", Compression.BROTLI, 8,
+                7, Format.ARROW_STREAM, props, 6, 5, "4321", Arrays.asList(queries), "select 2", 3, true);
+        Assert.assertEquals(args.loopCount, 10L);
+        Assert.assertEquals(args.loopInterval, 9L);
+        Assert.assertTrue(args.noProperties);
+        Assert.assertEquals(args.outputFile, "12.3");
+        Assert.assertEquals(args.outputFormat, Format.ARROW_STREAM);
+        Assert.assertEquals(args.outputParams, props);
+        Assert.assertEquals(args.outputCompression, Compression.BROTLI);
+        Assert.assertEquals(args.compressionLevel, 8);
+        Assert.assertEquals(args.compressionBuffer, 7);
+        Assert.assertEquals(args.tasks, 6);
+        Assert.assertEquals(args.taskCheckInterval, 5);
+        Assert.assertEquals(args.url, "4321");
+        Assert.assertEquals(args.queries.toArray(new String[2][]), queries);
+        Assert.assertEquals(args.validationQuery, "select 2");
+        Assert.assertEquals(args.validationTimeout, 3);
+        Assert.assertTrue(args.verbose);
+    }
+
+    @Test(groups = { "unit" })
+    public void testDefaultArguments() throws IOException {
+        Main.Arguments args = new Main.Arguments(new String[] { "new" });
+        Assert.assertEquals(args.loopCount, 1L);
+        Assert.assertEquals(args.loopInterval, 0L);
+        Assert.assertEquals(args.noProperties, false);
+        Assert.assertEquals(args.outputFile, "");
+        Assert.assertEquals(args.outputFormat, Format.TSV);
+        Assert.assertEquals(args.outputParams, new Properties());
+        Assert.assertEquals(args.outputCompression, Compression.NONE);
+        Assert.assertEquals(args.compressionLevel, -1);
+        Assert.assertEquals(args.compressionBuffer, 0);
+        Assert.assertEquals(args.tasks, 1);
+        Assert.assertEquals(args.taskCheckInterval, 10);
+        Assert.assertEquals(args.url, "new");
+        Assert.assertEquals(args.queries, Collections.emptyList());
+        Assert.assertEquals(args.validationQuery, "");
+        Assert.assertEquals(args.validationTimeout, 3);
+        Assert.assertEquals(args.verbose, false);
+    }
+
+    @Test(groups = { "unit" })
+    public void testNewArguments() throws IOException {
+        Main.Arguments args = new Main.Arguments(new String[1]);
+        Main.Arguments newArgs = new Main.Arguments(args, Collections.singletonList(new String[] { "1", "q1" }));
+        Assert.assertTrue(args.loopCount == newArgs.loopCount);
+        Assert.assertTrue(args.loopInterval == newArgs.loopInterval);
+        Assert.assertTrue(args.noProperties == newArgs.noProperties);
+        Assert.assertTrue(args.outputFile == newArgs.outputFile);
+        Assert.assertTrue(args.outputFormat == newArgs.outputFormat);
+        Assert.assertTrue(args.outputParams == newArgs.outputParams);
+        Assert.assertTrue(args.outputCompression == newArgs.outputCompression);
+        Assert.assertTrue(args.compressionLevel == newArgs.compressionLevel);
+        Assert.assertTrue(args.compressionBuffer == newArgs.compressionBuffer);
+        Assert.assertTrue(args.tasks == newArgs.tasks);
+        Assert.assertTrue(args.taskCheckInterval == newArgs.taskCheckInterval);
+        Assert.assertTrue(args.url == newArgs.url);
+        Assert.assertTrue(args.validationQuery == newArgs.validationQuery);
+        Assert.assertTrue(args.validationTimeout == newArgs.validationTimeout);
+        Assert.assertTrue(args.verbose == newArgs.verbose);
+
+        Assert.assertNotEquals(args.queries, newArgs.queries);
+        Assert.assertEquals(newArgs.queries.toArray(new String[1][]), new String[][] { { "1", "q1" } });
+    }
+
+    @Test(groups = { "unit" })
     public void testCloseConnection() throws SQLException {
         Main.closeQuietly(null);
         Main.closeQuietly(new ByteArrayInputStream(new byte[1]));
 
-        try (Connection conn = DriverManager.getConnection("jdbcx:sqlite::memory:", null)) {
+        try (Connection conn = DriverManager.getConnection(DEFAULT_CONNECTION_URL, null)) {
             Assert.assertFalse(conn.isClosed());
             Main.closeQuietly(conn);
             Assert.assertTrue(conn.isClosed());
         }
     }
 
-    @Test(groups = { "private" })
-    public void testGetOrCreateConnection() throws SQLException {
-        Assert.assertNull(Main.getOrCreateConnection(null, null, null, -1, null, false));
-        Assert.assertNull(Main.getOrCreateConnection(null, null, null, 0, null, true));
+    @Test(groups = { "unit" })
+    public void testExtractQueries() throws IOException {
+        Assert.assertEquals(
+                Main.Arguments.extractQueries(new String[] { "url", "", "@?iles", "select 1", "@1.sql" })
+                        .toArray(new String[0][]),
+                new String[][] { { "Query #1", "select 1" } });
+    }
 
-        Connection conn = Main.getOrCreateConnection("jdbcx:sqlite::memory:", null, null, 1000, "select 2",
-                false);
+    @Test(groups = { "unit" })
+    public void testGetOrCreateConnection() throws SQLException {
+        Assert.assertNull(Main.getOrCreateConnection(null, null, null, -1, null));
+        Assert.assertNull(Main.getOrCreateConnection(null, null, null, 0, null));
+
+        Connection conn = Main.getOrCreateConnection(DEFAULT_CONNECTION_URL, null, null, 1000, "select 2");
         Assert.assertNotNull(conn);
         for (int i = 0; i < 3; i++) {
             Assert.assertFalse(conn.isClosed());
@@ -67,17 +146,14 @@ public class MainTest {
                 props = new Properties();
             }
             Assert.assertEquals(conn,
-                    Main.getOrCreateConnection("jdbcx:sqlite::memory:", props, conn, 1000,
-                            "select 2", false));
+                    Main.getOrCreateConnection(DEFAULT_CONNECTION_URL, props, conn, 1000, "select 2"));
         }
 
         for (int i = 0; i < 100; i++) {
             Assert.assertFalse(conn.isClosed());
             conn.close();
             Assert.assertTrue(conn.isClosed());
-            Connection newConn = Main.getOrCreateConnection("jdbcx:sqlite::memory:", null, conn, 1000,
-                    "select 3",
-                    false);
+            Connection newConn = Main.getOrCreateConnection(DEFAULT_CONNECTION_URL, null, conn, 1000, "select 3");
             Assert.assertNotEquals(conn, newConn);
             Assert.assertTrue(conn.isClosed());
             Assert.assertFalse(newConn.isClosed());
@@ -120,53 +196,49 @@ public class MainTest {
                 new String[][] { { "n1", "q1" }, { "n3", "q3" }, { "n2", "q2" } });
     }
 
-    @Test(groups = { "private" })
+    @Test(groups = { "unit" })
     public void testExecuteQueries() throws IOException, SQLException {
-        final String url = "jdbcx:sqlite::memory:";
-        Assert.assertFalse(Main.executeQueries(url, false, 1000, null, null, null, null,
-                false));
-        Assert.assertFalse(Main.executeQueries(url, false, 1000, null,
-                Collections.emptyList(), null,
-                null, false));
+        final String url = DEFAULT_CONNECTION_URL;
+        final Main.Arguments args = new Main.Arguments(new String[] { url });
+        Assert.assertTrue(Main.executeQueries(args, null));
+        Assert.assertTrue(Main.executeQueries(new Main.Arguments(args, Collections.emptyList()), null));
 
-        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
-                Collections.singletonList(new String[] { "first", "select 1" }), null, null, false));
-        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
-                Collections.singletonList(
-                        new String[] { "first",
-                                "SELECT name FROM sqlite_master WHERE type='invalid_table'" }),
-                null, null, false));
+        Assert.assertTrue(Main.executeQueries(
+                new Main.Arguments(args, Collections.singletonList(new String[] { "first", "select 1" })), null));
+        Assert.assertTrue(Main.executeQueries(new Main.Arguments(args, Collections.singletonList(
+                new String[] { "first",
+                        "SELECT name FROM sqlite_master WHERE type='invalid_table'" })),
+                null));
         Assert.assertThrows(SQLException.class,
-                () -> Main.executeQueries(url, false, 1000, null,
-                        Collections.singletonList(new String[] { "first", "select '" }), null,
-                        null, false));
+                () -> Main.executeQueries(
+                        new Main.Arguments(args, Collections.singletonList(new String[] { "first", "select '" })),
+                        null));
 
-        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
-                Arrays.asList(new String[] { "first", "select 1" }, new String[] { "2nd", "select 2" }),
-                null, null,
-                false));
-        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
-                Arrays.asList(new String[] { "first",
-                        "SELECT name FROM sqlite_master WHERE type='invalid_table'" },
-                        new String[] { "2nd", "select 2" }),
-                null, null, false));
-        Assert.assertTrue(Main.executeQueries(url, false, 1000, null,
-                Arrays.asList(new String[] { "ddl",
-                        "create table if not exists test_execute_queries(a VARCHAR(30))" },
-                        new String[] { "1st",
-                                "SELECT name FROM sqlite_master WHERE type='test_execute_queries'" },
-                        new String[] { "2nd", "insert into test_execute_queries values('x')" },
-                        new String[] { "3rd", "select a from test_execute_queries" }),
-                null, null, true));
+        Assert.assertTrue(Main.executeQueries(
+                new Main.Arguments(args, Arrays.asList(new String[] { "first", "select 1" }, new String[] { "2nd",
+                        "select 2" })),
+                null));
+        Assert.assertTrue(Main.executeQueries(new Main.Arguments(args, Arrays.asList(new String[] { "first",
+                "SELECT name FROM sqlite_master WHERE type='invalid_table'" },
+                new String[] { "2nd", "select 2" })), null));
+        Assert.assertTrue(Main.executeQueries(new Main.Arguments(args, Arrays.asList(new String[] { "ddl",
+                "create table if not exists test_execute_queries(a VARCHAR(30))" },
+                new String[] { "1st",
+                        "SELECT name FROM sqlite_master WHERE type='test_execute_queries'" },
+                new String[] { "2nd", "insert into test_execute_queries values('x')" },
+                new String[] { "3rd", "select a from test_execute_queries" })), null));
     }
 
-    @Test(groups = { "private" })
+    @Test(groups = { "unit" })
     public void testExecuteQueriesInParallel() throws Exception {
         System.setProperty("verbose", "true");
-        // Main.main(new String[] { "jdbcx:sqlite::memory:",
-        // "@target/test-classes/test-queries/sequential" });
+        Assert.assertEquals(
+                Main.process(new String[] { DEFAULT_CONNECTION_URL, "@target/test-classes/test-queries/sequential" }),
+                0);
 
         System.setProperty("tasks", "2");
-        Main.main(new String[] { "jdbcx:sqlite::memory:", "@target/test-classes/test-queries/parallel" });
+        Assert.assertEquals(
+                Main.process(new String[] { DEFAULT_CONNECTION_URL, "@target/test-classes/test-queries/parallel" }),
+                0);
     }
 }
