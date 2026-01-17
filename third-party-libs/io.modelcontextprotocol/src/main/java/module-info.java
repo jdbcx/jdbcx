7898e34f7441b7c568f47d66f057aa47487233b0
Zhichun Wu <zhicwu@gmail.com>
2026-01-17 19:28:08 +0800

    repack mcp sdk due to modelcontextprotocol/java-sdk#560 (#105)

---
+++ third-party-libs/io.modelcontextprotocol/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module io.modelcontextprotocol {
+    exports io.modelcontextprotocol;
+    exports io.modelcontextprotocol.client;
+    exports io.modelcontextprotocol.client.transport;
+    exports io.modelcontextprotocol.common;
+    exports io.modelcontextprotocol.json;
+    exports io.modelcontextprotocol.server;
+    exports io.modelcontextprotocol.spec;
+}
