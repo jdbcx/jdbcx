f744d6da38e429fe449dca3e885aeff0da2a4117
Zhichun Wu <zhicwu@gmail.com>
2026-01-17 19:26:00 +0800

    repack third party libraries

---
+++ third-party-libs/io.modelcontextprotocol/src/main/java/io/modelcontextprotocol/DummyClass.java
@@ -0,0 +1,12 @@
+package io.modelcontextprotocol;
+
+/**
+ * Dummy class as placeholder. It will be removed during package phase.
+ */
+public class DummyClass {
+    /**
+     * Default constructor.
+     */
+    public DummyClass() { // NOSONAR
+    }
+}
